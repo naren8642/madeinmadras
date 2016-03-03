b68a7a4264262995abbb573a30dda47ced1e613d
Naren Salem <naren.salem@gmail.com>
2016-03-03 15:29:19 -0600

    Initial commit for MIM project

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yoyoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yoyoApp
+ */
+angular.module('yoyoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
