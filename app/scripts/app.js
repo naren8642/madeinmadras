a2825372e724a42e56e7b35652693b4ac46b7bcd
Naren Salem <naren.salem@gmail.com>
2016-03-04 16:08:19 -0600

    Showcase layout and title animation - almost

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,7 +8,7 @@
  *
  * Main module of the application.
  */
-angular
+var yoyoApp = angular
   .module('yoyoApp', [
     'ngAnimate',
     'ngCookies',
@@ -33,3 +33,33 @@ angular
         redirectTo: '/'
       });
   });
+
+yoyoApp.controller('ShowCaseController', function ($scope) {
+    $scope.animate = [];
+
+    function genBrick(name) {
+      var height = Math.floor(Math.random() * 500) + 100;
+      var width = 300 ; // Math.floor(Math.random() * 500) + 100;
+      var id = Math.floor(Math.random() * 10000);
+      return {
+        id: id,
+        name: name,
+        width: width,
+        height: height,
+        src: 'http://lorempixel.com/g/'+width+'/' + height + '/?' + id
+      };
+    }
+
+    $scope.row1 = [genBrick('Mathews Goff'), genBrick('Collins Alston'),genBrick('Jasmine Rollins'),genBrick('Julie Jefferson')];
+    $scope.row2 = [genBrick('Wilder King'), genBrick('Katie Moore'),genBrick('Reynolds Bishop'),genBrick('Felicity Flowers')];
+    $scope.row3 = [genBrick('Jenifer Martinez'), genBrick('Jason Perry'),genBrick('Destiny Mullins'),genBrick('Julie Jefferson')];
+
+  });
+
+yoyoApp.directive('animateName', function() {
+  return function(scope, element, attrs) {
+    scope.$watch(attrs.animateName, function() {
+      element.slideToggle("fast");
+    })
+  }
+});
